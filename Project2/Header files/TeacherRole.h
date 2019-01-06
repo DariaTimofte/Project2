7a53e48c9fd0d784abd550235a459ce4d3dff55a
Daria Timofte <36560235+DariaTimofte@users.noreply.github.com>
2019-01-07 01:53:53 +0200

    Add files via upload

---
+++ Project2/Header files/TeacherRole.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Role.h"
+
+class TeacherRole:public Role {
+public:
+    TeacherRole();
+    TeacherRole(std::string studies, std::string didacticFunction);
+    std::string getStudies();
+    std::string getDidacticFunction();
+    void setStudies(std::string studies);
+    void setDidacticFunction(std::string didacticFunction);
+private:
+    std::string mStudies;
+    std::string mDidacticFunction;
+};
